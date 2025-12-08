391ddefe57588496e8d4c52b6d85edfc2cf18ab4
Lizah-01 <118897068+Lizah-01@users.noreply.github.com>
2025-12-08 15:47:43 +0100

    Add files via upload

---
+++ reconstruction.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+#SBATCH --job-name="3D reconstruction" 
+#SBATCH --time=100:00:00 
+#SBATCH --ntasks=1 
+#SBATCH --account=research-as-bn 
+#SBATCH --cpus-per-task=6 
+#SBATCH --partition=compute-p2
+#SBATCH --mem-per-cpu=16G
+#SBATCH --output=output_%j.out
+
+apptainer pull docker://glyg/disperse:latest
+
+chmod u+x disperse_commands.sh
+
+srun disperse_commands.sh {{fits_filename}} {{persistence_val}} {{smooth_val}} {{assemble_val}} {{trimBelow_val}} {{sample_dir}}}
